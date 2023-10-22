634751e1bf69d5fcc61c230f494ea3fa6d066ea2
NellyNell <129366078+TekNellCo@users.noreply.github.com>
2023-10-21 21:15:45 -0400

    initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles/login.css";
+const body = document.getElementById("body");
+const splash_screen = document.createElement("div");
+
+splash_screen.className = "splash_screen";
+splash_screen.textContent = "test";
+body.append(splash_screen);
